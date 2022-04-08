1ea46324f2f978899c480141293783b04c0d7574
César <96417365+cesarra42@users.noreply.github.com>
2022-04-08 07:13:50 +0000

    EL 4

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+var A=10;
+var B=20;
+document.write("<p>Que "  ,A, " es menor que " ,B, " Y que " ,B, " es menor que " ,A, " es: " ,A<B&&B<A,"</p>") 
+document.write("<p>Que "  ,A, " es menor que " ,B, " o que " ,B, " es menor que " ,A, " es: " ,A<B||B<A,"</p>")
+document.write("<p>Que "  ,A, " es menor que " ,B, " Y que " ,B, " es menor que " ,A, " es: " ,A<B&&B<A," salvo que empleemos el operador ! delante de la expresión, entonces lo negaremos y se convertirá en: " ,!A<B&&B<A,"</p>") 
