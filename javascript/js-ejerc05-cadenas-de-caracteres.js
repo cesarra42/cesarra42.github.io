a9622cf248cff19e07d50a60986c49083821faa1
César <96417365+cesarra42@users.noreply.github.com>
2022-04-27 07:13:37 +0000

    ejercicio 5

---
+++ javascript/js-ejerc05-cadenas-de-caracteres.js
@@ -0,0 +1,5 @@
+var nombre="César"
+var apellidos= "Rodríguez Álvarez"
+var primera_letra= nombre[0]
+document.write("<p>Las variables nombre y apellidos encadenadas resultan ",nombre,"",apellidos,"</p>")
+document.write("<p>La primera letra de mi nombre es ", primera_letra,"</p>")
