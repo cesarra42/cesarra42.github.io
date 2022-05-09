1228fca6c670f1a18ea444e45af4ed9fe9aeab4c
César <96417365+cesarra42@users.noreply.github.com>
2022-05-09 09:19:35 +0000

    examen final

---
+++ javascript/examen.js
@@ -0,0 +1,19 @@
+alert("bienvenido al examen final")
+var nombre= prompt("Introduzca su nombre")
+alert ("Hola " +nombre)
+if(nombre[0]== "a"||nombre[0]== "e"||nombre[0]== "i"||nombre[0]== "0"||nombre[0]== "u"){
+  document.write("<p>La primera letra de tu nombre es una vocal</p>")}
+else
+{document.write("<p>La primera letra de tu nombre es una consonante.</p>")}
+
+
+
+
+
+
+
+
+
+
+
+  
