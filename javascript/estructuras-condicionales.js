32185ca2a15e820ee10e815554cc9b7bb5701b3a
César <96417365+cesarra42@users.noreply.github.com>
2022-05-11 07:05:59 +0000

    ejercicio 07

---
+++ javascript/estructuras-condicionales.js
@@ -0,0 +1,34 @@
+var culpable = window.prompt("¿eres culpable?"); // si, no 
+
+  
+
+if(culpable=="si"){ 
+
+document.write("<p>irás a la cárcel</p>"); 
+
+}else if(culpable=="no"){ 
+
+document.write("<p>irás a casa</p>"); 
+
+}else{ 
+
+document.write("<p>la documentación por favor</p>"); 
+
+} 
+var culpable = window.prompt("¿Vas a ir a la universidad?"); // si, no 
+
+  
+
+if(culpable=="si"){ 
+
+document.write("<p>Seguro que te a bien</p>"); 
+
+}else if(culpable=="no"){ 
+
+document.write("<p>Te recomiendo hacer un módulo</p>"); 
+
+}else{ 
+
+document.write("<p>No te he entendido</p>"); 
+
+} 
